811fdbcd1937c702dcb22082cbeeab5df1089631
Tom Raithel <t.raithel@googlemail.com>
2020-05-28 21:37:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const filePath = path.join(__dirname, process.env.SENSOR_FILE);
+
+const getTemp = (data) => {
+  const [, sensorData] = data.split("\n");
+  const [, temp] = sensorData.split("t=");
+  return parseInt(temp, 10) / 1000;
+};
+
+const run = async () => {
+  const data = await readFile(filePath, { encoding: "utf-8" });
+  const temp = getTemp(data);
+  console.log(`${temp} °C`);
+};
+
+run();
