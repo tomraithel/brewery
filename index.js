0cd558c9e4d168371c4ade6b79126478d3183304
Tom Raithel <t.raithel@googlemail.com>
2020-05-28 21:58:55 +0200

    Make a little server to display the temperature

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
-const filePath = path.resolve(process.env.SENSOR_FILE);
+const express = require("express");
 
-const getTemp = (data) => {
+const getTemperature = async () => {
+  const filePath = path.resolve(process.env.SENSOR_FILE);
+  const data = await readFile(filePath, { encoding: "utf-8" });
+
   const [, sensorData] = data.split("\n");
   const [, temp] = sensorData.split("t=");
   return parseInt(temp, 10) / 1000;
 };
 
+const startServer = () => {
+  const app = express();
+
+  app.get("/", async (req, res) => {
+    const temp = await getTemperature();
+    res.set("Refresh", "1; url=/");
+    res.send(`${temp} °C`);
+  });
+
+  app.listen(3000, function () {
+    console.log("Example app listening on port 3000!");
+  });
+};
+
 const run = async () => {
-  const data = await readFile(filePath, { encoding: "utf-8" });
-  const temp = getTemp(data);
-  console.log(`${temp} °C`);
+  startServer();
 };
 
 run();
